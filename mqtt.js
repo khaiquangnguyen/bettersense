5e1b83642bcd88ae0b3f10511494700b17f61c3c
Khai Nguyen <kqn001@bucknell.edu>
2018-11-27 22:52:56 -0800

    working

---
+++ mqtt.js
@@ -0,0 +1,72 @@
+
+var video = document.getElementById('video');
+var canvas = document.getElementById('motion');
+var score = document.getElementById('score');
+
+function initSuccess() {
+	DiffCamEngine.start();
+}
+
+function initError() {
+	alert('Something went wrong.');
+}
+
+DiffCamEngine.init({
+	video: video,
+	motionCanvas: canvas,
+	initSuccessCallback: initSuccess,
+	initErrorCallback: initError,
+	captureCallback: capture
+});
+  // Create a client instance
+  client = new Paho.MQTT.Client("m15.cloudmqtt.com", 36819,"web_" + parseInt(Math.random() * 100, 10));
+  //Example client = new Paho.MQTT.Client("m11.cloudmqtt.com", 32903, "web_" + parseInt(Math.random() * 100, 10));
+
+  // set callback handlers
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+  var options = {
+    useSSL: true,
+    userName: "vswyqwhc",
+    password: "G4Mx5iGkb5Ol",
+    onSuccess:onConnect,
+    onFailure:doFail
+  }
+
+  // connect the client
+  client.connect(options);
+
+  // called when the client connects
+  function onConnect() {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("onConnect");
+    client.subscribe("/bettersense");
+    message = new Paho.MQTT.Message("Hello CloudMQTT");
+    message.destinationName = "/bettersense";
+    client.send(message);
+  }
+
+  function doFail(e){
+    console.log(e);
+  }
+
+  // called when the client loses its connection
+  function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:"+responseObject.errorMessage);
+    }
+  }
+
+  // called when a message arrives
+  function onMessageArrived(message) {
+    console.log("onMessageArrived:"+message.payloadString);
+  }
+
+  function capture(payload) {
+    if (payload.score > 500){
+    score.textContent = payload.score;
+    message = new Paho.MQTT.Message("1");
+    message.destinationName = "/bettersense";
+    client.send(message);
+	}
+}
